2e26f0457d4a7b8332a59a4f3b21758f0b36a001
samiciit <sami.akram@digitalnet.com>
2019-12-02 17:07:04 +0500

    unable to login with tenant users 2

--- tenant_tutorial/middleware.py
+++ tenant_tutorial/middleware.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.db import connection
+from django.db import connection, utils
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
 from django.utils.deprecation import MiddlewareMixin  # todo change
@@ -15,32 +15,22 @@ class TenantMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
         connection.set_schema_to_public()
+        request.urlconf = settings.PUBLIC_SCHEMA_URLCONF
         hostname_without_port = remove_www_and_dev(request.get_host().split(':')[0])
-        selected_schema_name = ''
-        tenant_model = get_tenant_model()
-
         if hostname_without_port.startswith('login.'):
-            selected_schema_name = 'public'
-        elif hostname_without_port == settings.TENANT_DOMAIN:
-            selected_schema_name = 'public'
+            hostname_without_port = hostname_without_port.replace('login.')
 
-        if selected_schema_name == 'public':
-            tenant = tenant_model.objects.filter(schema_name='public').first()
-            if tenant:
-                request.tenant = tenant
-                connection.set_tenant(request.tenant)
-                ContentType.objects.clear_cache()
-            request.urlconf = settings.PUBLIC_SCHEMA_URLCONF
-            return
+        tenant = get_tenant_model().objects.filter(domain_url=hostname_without_port)
+        if tenant:
+            request.tenant = tenant[0]
+            request.urlconf = settings.ROOT_URLCONF
+        else:
+            if hostname_without_port == settings.TENANT_DOMAIN:
+                request.urlconf = settings.PUBLIC_SCHEMA_URLCONF
+                return
 
-        tenant = tenant_model.objects.filter(domain_url=hostname_without_port).first()
-        if not tenant:
-            res = {
-                'error': 'Subdomain ' + hostname_without_port + ' does not exist',
-                'status': 'Invalid Client'
-            }
-            return render(request, '404.html', res)
-
-        request.tenant = tenant
         connection.set_tenant(request.tenant)
         ContentType.objects.clear_cache()
+
+        if hostname_without_port == settings.TENANT_DOMAIN:
+            request.urlconf = settings.PUBLIC_SCHEMA_URLCONF
