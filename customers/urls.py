5ae71b2931348d8749d8fde4a5aa67a9db5400a1
samiciit <sami.akram@digitalnet.com>
2019-12-03 19:00:28 +0500

    changing urls

--- customers/urls.py
+++ customers/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from customers.views import TenantView, Delete, Create
 
 urlpatterns = [
--- tenant_tutorial/urls_public.py
+++ tenant_tutorial/urls_public.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.contrib import admin
 from django.conf.urls import include, url
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    url('', include('customers.urls')),
+    url('', include('website.urls')),
+    url('clients/', login_required(include('customers.urls'))),
     url('stripe/', include('my_stripe.urls')),
     path('admin/', admin.site.urls),
 ]
