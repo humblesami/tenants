dc393cf289eaee867fe7cad724ee301738ac4355
samiciit <sami.akram@digitalnet.com>
2019-12-06 20:15:04 +0500

    admin templates activated

--- main_app/settings.py
+++ main_app/settings.py
@@ -74,7 +74,6 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates')
-
         ],
         'APP_DIRS': True,
         'OPTIONS': {
